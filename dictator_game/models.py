c51a5a431cfa563c6eda783f5ffc476ca687502f
DataTeller <151622487+morbifakhar69@users.noreply.github.com>
2025-07-29 01:00:13 +0200

    Update models.py
    added app name

--- dictator_game/models.py
+++ dictator_game/models.py
@@ -52,6 +52,9 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
+
+    app_name = models.StringField(initial='goaloriented_del1st')
+
     final_allocations = models.LongStringField()
     conversation_history=models.LongStringField(initial='[]') 
     orientation_history=models.LongStringField(initial='{}')
